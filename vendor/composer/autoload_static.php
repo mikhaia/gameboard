a8120e9b217c5fd9f840e0d244ef8879a0e41b94
Alexandr Mikhailov <alexander@enclave.vn>
2023-10-05 14:39:28 +0700

    Fix issue with loading main screen after login

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -322,8 +322,8 @@ class ComposerStaticInit3855318690b5ea148bba9c89dc2d5646
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'abe8cc81537947f7588dcee299d2f5bea928bda5',
+        'reference' => 'd552c1790b3c8f99639d9440640a228665c72e5b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -391,7 +391,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'abe8cc81537947f7588dcee299d2f5bea928bda5',
+            'reference' => 'd552c1790b3c8f99639d9440640a228665c72e5b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
